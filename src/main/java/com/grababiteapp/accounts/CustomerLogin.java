4e1f978df6217377088c9061b17de6ae556da3d2
Bhuvanesh <Bhuvanesh01@users.noreply.github.com>
2021-04-08 15:48:06 +0530

    Initial Commit

---
+++ src/main/java/com/grababiteapp/accounts/CustomerLogin.java
@@ -0,0 +1,5 @@
+package com.grababiteapp.accounts;
+
+public class CustomerLogin {
+
+}
---
+++ src/main/java/com/grababiteapp/accounts/RestaurantLogin.java
@@ -0,0 +1,10 @@
+package com.grababiteapp.accounts;
+
+public class RestaurantLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
