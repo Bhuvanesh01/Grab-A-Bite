f50e90daf0a34770842c649b2d23488e54421fc5
sharmishtha <sharmishtha@gmail.com>
2021-04-09 10:13:11 +0530

    add restaturant service

--- src/main/java/com/grababiteapp/service/RestaurantServiceImpl.java
+++ src/main/java/com/grababiteapp/service/RestaurantServiceImpl.java
@@ -3,8 +3,41 @@ package com.grababiteapp.service;
 public class RestaurantServiceImpl implements RestaurantService {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+
+	public void addFoodItem() {
+		
+		
+	}
+
+	public void deleteFoodItem(int foodItemId) {
+		
+		
+	}
+
+	public void updateFoodItemDetails(int foodItemId, double price) {
+		
+	}
+
+	public void acceptOrder(int orderId) {
+		
+	}
+
+	public void rejectOrder(int orderId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showProfile() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void changeOrderStatus(int orderId) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
